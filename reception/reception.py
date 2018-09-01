637218559ff212301d6d084fe44539be017a273f
KicaRonaldOkello <okello.ronald045@gmail.com>
2018-09-01 10:04:06 +0300

    Follow PEP8 standard

--- reception/reception.py
+++ reception/reception.py
@@ -1,27 +1,28 @@
 
-vip = []
-ordinary = []
+VIP = []
+ORDINARY = []
 
 
 def registration_checker(name1):
+    """Function to check for name from guest lists."""
     file1 = open('vip_list.txt', 'r')
     for line in file1:
-        vip.append(line.strip())
+        VIP.append(line.strip())
 
     file2 = open('ordinary_list.txt', 'r')
     for line in file2:
-        ordinary.append(line.strip())
+        ORDINARY.append(line.strip())
 
     if name1 == "":
         print ("Please enter a valid name")
     else:
-        name = [a for a in vip if name1.lower() in a.lower()]
-        ord = [a for a in ordinary if name1.lower() in a.lower()]
-    
+        name = [a for a in VIP if name1.lower() in a.lower()]
+        ordin = [a for a in ORDINARY if name1.lower() in a.lower()]
+
         if name:
             print (name[0] + " is on VIP")
         elif ord:
-            print (ord[0] + " is on ordinary")
+            print (ordin[0] + " is on ordinary")
         else:
             print (name1 +" is not on the list")
 
